14022d71c4d03059d6ef7d602989b27981830498
LanZhisheng <kevinlan@aijiatui.com>
2019-01-07 15:48:59 +0800

    feat:自定义输入框

---
+++ src/page/richText/CustomEditor.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="custom-editor">
+    <div class="editor-inp" ref="customEditor" contenteditable="true"></div>
+
+    <div class="editor-img">
+      <el-upload
+        class="upload-demo"
+        action
+        :before-upload="beforeAvatarUpload"
+        :http-request="httpRequest"
+      >
+        <el-button size="small" type="primary">点击上传</el-button>
+        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+      </el-upload>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "customEditor",
+
+  data() {
+    return {};
+  },
+
+  methods: {
+    beforeAvatarUpload() {
+      console.log("customEditor值为", this.$refs.customEditor);
+      this.$refs.customEditor.innerHTML +=
+        '<img class="inp-img" src="/static/img/avactor.66c4498.png">';
+    },
+
+    httpRequest() {}
+  }
+};
+</script>
+
+<style lang='scss'>
+.custom-editor {
+  width: 500px;
+  height: 500px;
+  .editor-inp {
+    height: 400px;
+    border: 1px solid #409eff;
+    overflow: auto;
+    img{
+      width: 300px;
+    }
+  }
+}
+  .inp-img {
+  }
+</style>
--- src/page/richText/TextEditor.vue
+++ src/page/richText/TextEditor.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="text-editor">
     <vue-editor v-model="content"></vue-editor>
     <span>{{content}}</span>
   </div>
@@ -7,28 +7,33 @@
 
 <script>
 import { VueEditor, Quill } from "vue2-editor";
-  export default {
-    name: 'UE',
-    components:{VueEditor},
-    data () {
-      return {
-        content: '<h1>我是标题1</h1>'
-      }
-    },
-    mounted() {
-    },
-    methods: {
-    },
-    destroyed() {
-    }
+import $ from "jquery";
+export default {
+  name: "UE",
+
+  components: { VueEditor },
+
+  data() {
+    return {
+      content: "<h1>我是标题1</h1>"
+    };
+  },
+
+  mounted() {},
+
+  methods: {
+  destroyed() {}
   }
+};
 </script>
 
-<style>
-.save-editor{
-  margin-left:70%;
-}
-.editor-text{
-  text-align: left;
+<style lang="scss">
+.text-editor {
+  .save-editor {
+    margin-left: 70%;
+  }
+  .editor-text {
+    text-align: left;
+  }
 }
 </style>
