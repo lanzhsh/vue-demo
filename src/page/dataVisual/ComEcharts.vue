9bae91ecb42b4dc5408f0c103a3a6dd4a0dc8001
LanZhisheng <kevinlan@aijiatui.com>
2019-03-13 12:10:29 +0800

    feat:图标公共类封装

--- src/page/dataVisual/ComEcharts.vue
+++ src/page/dataVisual/ComEcharts.vue
@@ -14,164 +14,43 @@
   width: 400px;
   height: 400px;
 }
+#pie-chart{
+  width: 400px;
+  height:200px;
+}
 </style>
 
 
 <script>
+import initEchart from '@/utils/mixin/chart';
 export default {
+  mixins:[initEchart],
   data() {
-    return {};
+    return {}
   },
   mounted() {
-    this.drawLine();
+    this.init();
   },
   methods: {
-    drawLine() {
+    init() {
       // 柱状图
-      let histogram = this.$echarts.init(document.getElementById("histogram"));
-      // 绘制图表
-      histogram.setOption({
-        title: { text: "ECharts 入门示例" },
-        tooltip: {},
-        xAxis: {
-          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-        },
-        yAxis: {},
-        series: [
-          {
-            name: "销量",
-            type: "bar",
-            data: [5, 20, 36, 10, 10, 20]
-          }
-        ]
-      });
+      this.drawbar(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+       [5, 20, 36, 10, 10, 20], "histogram");
 
       //扇形图
-      let pieChart = this.$echarts.init(document.getElementById("pie-chart"));
-      pieChart.setOption({
-        title: {
-          text: "某站点用户访问来源",
-          subtext: "纯属虚构",
-          x: "center"
-        },
-        tooltip: {
-          trigger: "item",
-          formatter: "{a} <br/>{b} : {c} ({d}%)"
-        },
-        legend: {
-          orient: "vertical",
-          left: "left",
-          data: ["直接访问", "邮件营销", "联盟广告", "视频广告", "搜索引擎"]
-        },
-        series: [
-          {
-            name: "访问来源",
-            type: "pie",
-            radius: "55%",
-            center: ["50%", "60%"],
-            data: [
-              { value: 335, name: "直接访问" },
-              { value: 310, name: "邮件营销" },
-              { value: 234, name: "联盟广告" },
-              { value: 135, name: "视频广告" },
-              { value: 1548, name: "搜索引擎" }
-            ],
-            itemStyle: {
-              emphasis: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: "rgba(0, 0, 0, 0.5)"
-              }
-            }
-          }
-        ]
-      });
+      let pieArr=[
+        { name: "男", percent: 40, number: 40 },
+        { name: "女", percent: 40, number: 40 },
+        { name: "未知", percent: 10, number: 10 },
+      ]
+      this.drawpie(...pieArr, 'pie-chart');
 
       //折线图
-      let lineChart = this.$echarts.init(document.getElementById("line-chart"));
-      lineChart.setOption({
-        title: {
-          text: "未来一周气温变化",
-          subtext: "纯属虚构"
-        },
-        tooltip: {
-          trigger: "axis"
-        },
-        legend: {
-          data: ["最高气温", "最低气温"]
-        },
-        toolbox: {
-          show: true,
-          feature: {
-            dataZoom: {
-              yAxisIndex: "none"
-            },
-            dataView: { readOnly: false },
-            magicType: { type: ["line", "bar"] },
-            restore: {},
-            saveAsImage: {}
-          }
-        },
-        xAxis: {
-          type: "category",
-          boundaryGap: false,
-          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"]
-        },
-        yAxis: {
-          type: "value",
-          axisLabel: {
-            formatter: "{value} °C"
-          }
-        },
-        series: [
-          {
-            name: "最高气温",
-            type: "line",
-            data: [11, 11, 15, 13, 12, 13, 10],
-            markPoint: {
-              data: [
-                { type: "max", name: "最大值" },
-                { type: "min", name: "最小值" }
-              ]
-            },
-            markLine: {
-              data: [{ type: "average", name: "平均值" }]
-            }
-          },
-          {
-            name: "最低气温",
-            type: "line",
-            data: [1, -2, 2, 5, 3, 2, 0],
-            markPoint: {
-              data: [{ name: "周最低", value: -2, xAxis: 1, yAxis: -1.5 }]
-            },
-            markLine: {
-              data: [
-                { type: "average", name: "平均值" },
-                [
-                  {
-                    symbol: "none",
-                    x: "90%",
-                    yAxis: "max"
-                  },
-                  {
-                    symbol: "circle",
-                    label: {
-                      normal: {
-                        position: "start",
-                        formatter: "最大值"
-                      }
-                    },
-                    type: "max",
-                    name: "最高点"
-                  }
-                ]
-              ]
-            }
-          }
-        ]
-      });
+      this.drawline(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+       [5, 20, 36, 10, 10, 20], "line-chart");
     }
   }
 };
+
+console.log('执行啦')
 </script>
